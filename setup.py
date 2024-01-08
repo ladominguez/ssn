45922067a0f1d4aa7b903c16372e73ce4f426517
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-01-08 16:53:04 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Luis A. Dominguez',
+    author_email='ladominguez@igeofisica.unam.mx',
+    description='SSN library',
+    url='https://github.com/ladominguez/ssn',  # Optional
+)
+
+
