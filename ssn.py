f8da5230aa61eb6451321f15f37ef6eb916c80e1
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-04-15 11:35:45 -0600

    Header option added.

--- ssn.py
+++ ssn.py
@@ -15,8 +15,8 @@ def get_station_by_name(name):
     df = get_all_stations()
     return df.loc[df['stnm'] == name]['latitude'].to_numpy()[0], df.loc[df['stnm'] == name]['longitude'].to_numpy()[0]
 
-def read_MF_file(match_filter_file):
-    df = pd.read_csv(match_filter_file, delim_whitespace=True, names = names, dtype = dtypes)
+def read_MF_file(match_filter_file, header = None):
+    df = pd.read_csv(match_filter_file, delim_whitespace=True, names = names, dtype = dtypes, header = header)
     df['Date'] = df['Date'].str.cat(df['Time'], sep=' ')
     df['Date'] = pd.to_datetime(df['Date'], format='%Y/%m/%d %H:%M:%S.%f')
     return df
